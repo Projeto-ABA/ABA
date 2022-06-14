b457c96722a9ef71bff4a4761fd2d22e139b15a3
Mirlla Marques Santana Alves <mirlla.alves@virtus.ufcg.edu.br>
2022-06-13 22:47:48 -0300

    SETTINGS

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {MatCardModule} from '@angular/material/card';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,7 +11,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
