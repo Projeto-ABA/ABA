9dbe8becb1ad380052048ca4e0a8d7069bb141c6
Mirlla Marques Santana Alves <mirlla.alves@virtus.ufcg.edu.br>
2022-06-20 21:07:34 -0300

    UPDATES

---
+++ client/src/app/home/logout/home-logout/home-logout.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home-logout',
+  templateUrl: './home-logout.component.html',
+  styleUrls: ['./home-logout.component.scss']
+})
+export class HomeLogoutComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
