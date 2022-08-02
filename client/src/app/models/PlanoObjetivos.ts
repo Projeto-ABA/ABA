533b775e6e54e62c3a0472f68fcd8bf89056ef68
Fabio Almendro <fabio.almendro@virtus.ufcg.edu.br>
2022-08-02 19:53:17 -0300

    criação de models de planos de objetivos

---
+++ client/src/app/models/PlanoObjetivos.ts
@@ -0,0 +1,5 @@
+export class PlanoObjetivos{
+    emailInstrutor!: string;
+    descrição!: string;
+    prazo!: string;
+}
--- client/src/app/planos/planos/planos.component.ts
+++ client/src/app/planos/planos/planos.component.ts
@@ -12,28 +12,7 @@ export class PlanosComponent implements OnInit {
 
   ngOnInit(): void {
   }
-  home() :void{
-    this.router.navigateByUrl('home');
-  }
-  turmas(): void{
-    this.router.navigateByUrl('turmas');
-  }
-  novoAluno(): void{
-    this.router.navigateByUrl('cadastro-alunos');
-  }
-  novaTurma(): void{
-    this.router.navigateByUrl('cadastro-turmas');
-  }
-  alunos(): void{
-    this.router.navigateByUrl('alunos');
-  }
-  planos(): void{
-    this.router.navigateByUrl('listar-planos');
-  }
-  novoPlano(): void{
-    this.router.navigateByUrl('planos');
-  }
-  perfil(): void{
-    this.router.navigateByUrl('perfil');
+  navega(caminho:string){
+    this.router.navigateByUrl(caminho);
   }
 }
